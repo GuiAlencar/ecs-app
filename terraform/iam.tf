9ec5cc845df6dbaf10c8e179c10362aeee76222d
Guilherme <uchihaguilherme4@gmail.com>
2025-09-02 23:04:53 -0300

    adiciona task execution role

---
+++ terraform/iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "main" {
+  name = format("%s-role", var.service_name)
+
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+  name = format("%s-policy", var.service_name)
+  role = aws_iam_role.main.id
+
+  policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "s3:GetObject",
+          "sqs:*",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+  
+}
